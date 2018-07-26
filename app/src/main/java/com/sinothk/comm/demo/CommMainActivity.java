bd8c5776ee0a8abf3bed65ebf6436030281464c4
LiangYutao <sinothk@126.com>
2018-07-26 16:30:50 +0800

    同步

--- app/src/main/java/com/sinothk/comm/demo/CommMainActivity.java
+++ app/src/main/java/com/sinothk/comm/demo/CommMainActivity.java
@@ -10,6 +10,5 @@ public class CommMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //  sdasdad
     }
 }
