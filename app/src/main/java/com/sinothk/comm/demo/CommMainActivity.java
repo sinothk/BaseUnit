1517c3d0bd4e2259d732616b6788e36bf46c6cf1
LiangYT <381518188@qq.com>
2018-09-20 14:15:52 +0800

    新增工具类测试

--- app/src/main/java/com/sinothk/comm/demo/CommMainActivity.java
+++ app/src/main/java/com/sinothk/comm/demo/CommMainActivity.java
@@ -5,20 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-import com.sinothk.comm.demo.dpMatch.ScreenMatchDemoActivity;
-import com.sinothk.comm.utils.StringUtil;
-
 public class CommMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 
     public void gotoScreenMatchDemoActivity(View view) {
         startActivity(new Intent(this, ScreenMatchDemoActivity.class));
+    }
+
+    public void gotoUtilsDemoActivity(View view) {
+        startActivity(new Intent(this, UtilsDemoActivity.class));
     }
 }
--- app/src/main/java/com/sinothk/comm/demo/UtilsDemoActivity.java
+++ app/src/main/java/com/sinothk/comm/demo/UtilsDemoActivity.java
@@ -1,15 +1,13 @@
-package com.sinothk.comm.demo.dpMatch;
+package com.sinothk.comm.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.sinothk.comm.demo.R;
-
-public class ScreenMatchDemoActivity extends AppCompatActivity {
+public class UtilsDemoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_screen_match_demo);
+        setContentView(R.layout.activity_utils_demo);
     }
 }
