4665f66dbf81d2ee79873c937272f6f0ebc39085
LiangYT <381518188@qq.com>
2018-10-24 11:05:43 +0800

    新增丢弃适配文件和汉字首字母获取功能！

--- app/src/main/java/com/sinothk/comm/demo/UtilsDemoActivity.java
+++ app/src/main/java/com/sinothk/comm/demo/UtilsDemoActivity.java
@@ -3,10 +3,19 @@ package com.sinothk.comm.demo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.sinothk.comm.demo.bean.UserBean;
+import com.sinothk.comm.utils.CollectionUtil;
 import com.sinothk.comm.utils.DateUtil;
 import com.sinothk.comm.utils.ToastUtil;
 
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 public class UtilsDemoActivity extends AppCompatActivity {
 
@@ -17,8 +26,21 @@ public class UtilsDemoActivity extends AppCompatActivity {
 
         ToastUtil.init(this);
 
-        int age = DateUtil.getAge(DateUtil.getDateByDateStr("1988-12-18", "yyyy-MM-dd"));
+        ArrayList<UserBean> users = new ArrayList<>();
+        users.add(new UserBean("北方", "男", 30));
+        users.add(new UserBean("汕头", "女", 31));
+        users.add(new UserBean("安庆", "男", 23));
+        users.add(new UserBean("南京", "男", 45));
 
-        ToastUtil.show("age = " + age);
+
+        Collections.sort(users, new Comparator<UserBean>() {
+            public int compare(UserBean o1, UserBean o2) {
+                return CollectionUtil.sortChineseFirstSpell(o1.getName()).compareTo(CollectionUtil.sortChineseFirstSpell(o2.getName()));
+            }
+        });
+
+        for (UserBean i : users) {
+            System.out.print(i.getName() + "  ");
+        }
     }
 }
